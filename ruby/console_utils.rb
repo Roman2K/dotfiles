473428815e42b7eacbbf15a0b2061026180a58c6
Roman Le Négrate <roman.lenegrate@gmail.com>
2015-03-17 15:36:00 +0100

    ruby: #bench: print the result

--- ruby/console_utils.rb
+++ ruby/console_utils.rb
@@ -1,5 +1,26 @@
+class NumberFormatter
+  UNITS = ['', *%w(k m g t)]
+
+  def initialize(multiple=1000, suffix='')
+    @multiple = multiple
+    @suffix = suffix
+  end
+
+  def format(n)
+    UNITS.each.with_index do |name, index|
+      in_unit = n.to_f / @multiple ** index
+      next if in_unit > @multiple && index < UNITS.size - 1
+      in_unit = ("%.1f" % in_unit).sub(/\.0$/, '')
+      return "#{in_unit}#{name}#{@suffix}"
+    end
+  end
+end
+
 def bench(runs=1_000_000)
-  t1 = Time.now
+  t0 = Time.now
   runs.times { yield }
-  Time.now - t1
+  (Time.now - t0).tap do |total|
+    f = NumberFormatter.new
+    puts "%s runs @ %s/s" % [f.format(runs), f.format(runs / total)]
+  end
 end

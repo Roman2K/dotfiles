7cbd5ba07e73e7481f0f46335771a8554adef434
Roman Le Négrate <roman.lenegrate@gmail.com>
2015-04-05 14:18:51 +0200

    ruby: #bench: print the time per iteration

--- ruby/console_utils.rb
+++ ruby/console_utils.rb
@@ -1,26 +1,30 @@
 class NumberFormatter
-  UNITS = ['', *%w(k m g t)]
-
-  def initialize(multiple=1000, suffix='')
+  def initialize(multiple, units)
     @multiple = multiple
-    @suffix = suffix
+    @units = units
   end
 
   def format(n)
-    UNITS.each.with_index do |name, index|
+    @units.each.with_index do |name, index|
       in_unit = n.to_f / @multiple ** index
-      next if in_unit > @multiple && index < UNITS.size - 1
+      next if in_unit > @multiple && index < @units.size - 1
       in_unit = ("%.1f" % in_unit).sub(/\.0$/, '')
       return "#{in_unit}#{name}#{@suffix}"
     end
   end
 end
 
+NUM_FMT = NumberFormatter.new(1000, ['', 'k', 'm', 'g', 't'])
+TIME_FMT = NumberFormatter.new(1000, ['µs', 'ms', 's'])
+
 def bench(runs=1_000_000)
   t0 = Time.now
   runs.times { yield }
   (Time.now - t0).tap do |total|
-    f = NumberFormatter.new
-    puts "%s runs @ %s/s" % [f.format(runs), f.format(runs / total)]
+    puts "%s runs @ %s/s | %s/iteration" % [
+      NUM_FMT.format(runs),
+      NUM_FMT.format(runs / total),
+      TIME_FMT.format(total / runs * 1_000_000),
+    ]
   end
 end
